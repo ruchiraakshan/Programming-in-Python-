114c57204112e6344132bbc1b4d4a43d58afef09
ruchira lakshan <ruchiralakshan2003@gmail.com>
2025-12-09 22:18:05 +0530

    Add files via upload

---
+++ formula_calc.py
@@ -0,0 +1,14 @@
+X = int(input()) 
+
+Y = X*4 + 14
+
+#--------TYPE YOUR CODE TO COMPLETE PROGRAM BELOW THIS LINE----------
+
+if Y > 30:
+    Z = Y * 4
+else:
+    Z = Y - 4
+
+#--------TYPE YOUR CODE TO COMPLETE PROGRAM ABOVE THIS LINE----------
+
+print(Z)
