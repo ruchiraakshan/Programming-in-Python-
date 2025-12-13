50d10edc6cc78258c2ed894ded932d86e09da160
ruchira lakshan <ruchiralakshan2003@gmail.com>
2025-12-13 23:14:03 +0530

    Add files via upload

---
+++ Functions - Exercise.py
@@ -0,0 +1,6 @@
+# Write a python3 function sqr( n ) that returns the square of its numeric parameter n.
+def sqr(n):
+    return n * n
+num = float(input("Enter a number: "))
+result = sqr(num)
+print("The square ->", num, "is", result)
